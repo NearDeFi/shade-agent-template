a64f4cf51cd76be5a6e087ca9ee043dbc62e66e1
PiVortex <owengeorgehassall@gmail.com>
2025-06-20 15:59:21 +0100

    fix some small issues

--- pages/api/getWorkerAccount.js
+++ pages/api/getWorkerAccount.js
@@ -1,12 +1,22 @@
-import { getAgentAccount, getBalance } from '@neardefi/shade-agent-js';
+import {getBalance, deriveWorkerAccount } from '@neardefi/shade-agent-js';
 
 export default async function handler(req, res) {
     try {
-        const accountId = await getAgentAccount();
-        console.log('Worker account:', accountId.workerAccountId);
-        const balance = await getBalance(accountId.workerAccountId);
-        console.log('Balance:', balance.available);
-        res.status(200).json({ accountId: accountId.workerAccountId, balance: balance.available });
+        if (process.env.NEXT_PUBLIC_accountId !== undefined) {
+            const balance = await getBalance(process.env.NEXT_PUBLIC_accountId);
+            res.status(200).json({ accountId: process.env.NEXT_PUBLIC_accountId, balance: balance.available });
+            return;
+        }
+      
+        // Add this check to prevent TEE operations in local dev
+        if (process.env.NODE_ENV !== 'production') {
+            throw new Error('TEE operations only available in production');
+        }
+    
+        const accountId = await deriveWorkerAccount();
+        const balance = await getBalance(accountId);
+
+        res.status(200).json({ accountId: accountId, balance: balance.available });
     } catch (error) {
         console.log('Error getting worker account:', error);
         res.status(500).json({ error: 'Failed to get worker account ' + error });
