db15ea1e818bec0906be77d782d99d26f771c1ac
PiVortex <owengeorgehassall@gmail.com>
2025-06-20 16:42:51 +0100

    need to fix replaceAll

--- pages/api/getWorkerAccount.js
+++ pages/api/getWorkerAccount.js
@@ -1,4 +1,4 @@
-import {getBalance, deriveWorkerAccount } from '@neardefi/shade-agent-js';
+import {getBalance, deriveAgentAccount } from '@neardefi/shade-agent-js';
 
 export default async function handler(req, res) {
     try {
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
             throw new Error('TEE operations only available in production');
         }
     
-        const accountId = await deriveWorkerAccount();
+        const accountId = await deriveAgentAccount();
         const balance = await getBalance(accountId);
 
         res.status(200).json({ accountId: accountId, balance: balance.available });
